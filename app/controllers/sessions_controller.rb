2da4df239880bc36f97007bc74dd4b4718c5a4ed
Meredith Reece <mardebugger@gmail.com>
2018-08-10 23:54:33 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:name])
+    user = User.find_by(name: params[:user][:name])
     if user && user.authenticate(params[:user][:name])
 
     end
