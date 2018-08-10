9eaf11c6fd120dba1fa1b831a5f4a21118702e3d
Meredith Reece <mardebugger@gmail.com>
2018-08-10 22:50:16 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180810222855) do
+ActiveRecord::Schema.define(version: 20180810222229) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "password_digest"
+    t.string   "password"
     t.string   "password_confirmation"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
