2bffe8a49f98b19df79272401c45906289e6e465
Meredith Reece <mardebugger@gmail.com>
2018-08-10 23:46:15 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,10 @@ class UsersController < ApplicationController
   def create
 
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
