b003abb850d5c0891b4b1760a4af02eaa5702299
Meredith Reece <mardebugger@gmail.com>
2018-08-11 00:00:57 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if !@user.save
-      redirect_to '/signup'
+      redirect_to '/login'
     else
       session[:user_id] = @user.id
       redirect_to '/home'
