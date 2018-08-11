f1641675cfd423fd04ef1575afd2e7227fd2f8d2
Meredith Reece <mardebugger@gmail.com>
2018-08-11 00:08:23 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:user][:name])
-    if user && user.authenticate(params[:user][:password]) && user.authenticate(params[:user][:password_confirmation])
-      session[:user_id] = user.id
-      redirect_to home_path(user)
+    if user && user.authenticate(params[:user][:password])
+      password_confirmation = params[:user][:password_confirmation]
+      if user.authenticate(password_confirmation)
+        session[:user_id] = user.id
+        redirect_to home_path(user)
+      end
+
     else
       flash[:error] = "Nope - try again."
       redirect_to login_path
